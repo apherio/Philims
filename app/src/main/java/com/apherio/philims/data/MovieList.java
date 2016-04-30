f152a9b77734811b46f3d9ab1198f256c5c75d9f
sahil <Saheel.mhapsekar@gmail.com>
2016-04-30 20:34:12 +0530

    Retrofit sort done-stage1

---
+++ app/src/main/java/com/apherio/philims/data/MovieList.java
@@ -0,0 +1,48 @@
+package com.apherio.philims.data;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by sahil on 28/04/16.
+ * Basic movie list fetcher from the movies DB api
+ */
+public class MovieList {
+    static MovieList sMovieDataList;
+    Context mContext;
+    private List<Movie> mResultsArrayList = new ArrayList<Movie>();
+
+    MovieList(Context context) {
+        mContext = context;
+    }
+
+    public static MovieList get(Context context) {
+        if (sMovieDataList == null) {
+            sMovieDataList = new MovieList(context);
+        }
+        return sMovieDataList;
+    }
+
+    public List<Movie> getResultsArrayList() {
+        return mResultsArrayList;
+    }
+
+    public void setResultsArrayList(List<Movie> resultsList) {
+        for (Movie Results : resultsList)
+            mResultsArrayList.add(Results);
+    }
+
+    public void setSingleResult(Movie result) {
+        mResultsArrayList.add(result);
+    }
+
+    public Movie getSingleResultByPosition(int pos) {
+        return mResultsArrayList.get(pos);
+    }
+
+    public void clearList() {
+        mResultsArrayList.clear();
+    }
+}
